1ad836d23ec9f85250018a81ca8c3c1bb563aee9
Serge Gusarov <mrhersergik@gmail.com>
2025-01-29 21:47:00 +0700

    reafctor + added classes for node js modules

---
+++ backend/nodejs/base/util_module/inherits.js
@@ -0,0 +1,23 @@
+var util = require('util');
+
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.walk = function() {
+    console.log("Ходит " + this.name);
+}
+
+function Rabbit(name) {
+    this.name = name;
+}
+
+util.inherits(Rabbit, Animal);
+
+Rabbit.prototype.jump = function() {
+    console.log("Прыгает " + this.name);
+}
+
+var rabbit = new Rabbit("наш кролик");
+rabbit.walk();
+rabbit.jump();
