45043d394fa14afdf3e1ab6b34575fa530b66c29
Serge Gusarov <mrhersergik@gmail.com>
2025-02-06 13:51:36 +0700

    css modules

---
+++ frontend/react/23-css-modules/src/components/Info.js
@@ -0,0 +1,15 @@
+import './Info.css';
+import styles from './Info.module.css';
+console.log(styles);
+
+const Info = () => {
+  return (
+    <div className={styles.info}>
+      <h1>Hello from the Info component</h1>
+      <h2>heading in the Info component</h2>
+      <button className="my-button">Click me in the Info component</button>
+    </div>
+  );
+};
+
+export default Info;
