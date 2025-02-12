2e47db91426a92f34f0b3a29f49a1ea48ba6ac78
Serge Gusarov <mrhersergik@gmail.com>
2025-02-12 13:08:35 +0700

    switch to object for store model

---
+++ frontend/react/25-todo-app-v2/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import TodoForm from './components/TodoList/TodoForm';
+import TodoList from './components/TodoList/TodoList';
+import './App.css';
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const addTodoItemHandler = (text) => {
+    const newTodoItem = {
+      text: text,
+      isCompleted: false,
+      id: uuidv4(),
+    }
+    setTodoList([...todoList, newTodoItem])
+  }
+  const deleteTodoItemHandler = (id) => {
+    setTodoList(todoList.filter((todoItem) => todoItem.id !== id))
+  }
+  return (
+    <div className="App">
+      <h1>Todo app</h1>
+      <TodoForm addTodoItem={addTodoItemHandler} />
+      <TodoList todoList={todoList} deleteTodoItem={deleteTodoItemHandler} />
+    </div>
+  );
+}
+
+export default App;
