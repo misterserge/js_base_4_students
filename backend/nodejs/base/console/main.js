2fb67cec691ba9c6ca46dfa366702fb255924a45
Serge Gusarov <mrhersergik@gmail.com>
2025-11-03 14:59:23 +0300

    console method

---
+++ backend/nodejs/base/console/main.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+console.error("Error: Hello World");
+console.warn("Warning: Hello World");
+console.info("Info: Hello World");
+console.debug("Debug: Hello World");
+console.log('Trace ================================');
+console.trace("Trace: Hello World");
+console.log('Trace ================================');
+console.dir(console);
+console.time("Time: Hello World");
+console.timeEnd("Time: Hello World");
+console.timeLog("Time: Hello World");
+console.timeEnd("Time: Hello World");
