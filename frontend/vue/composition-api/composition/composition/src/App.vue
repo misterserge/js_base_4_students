5032683449b8babbc6a2fc6d89a89b04bac452cd
Serge Gusarov <mrhersergik@gmail.com>
2025-11-05 16:47:33 +0300

    vue composition api

---
+++ frontend/vue/composition-api/composition/composition/src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <h1 class="text-3xl font-bold underline">    Hello world!  </h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
